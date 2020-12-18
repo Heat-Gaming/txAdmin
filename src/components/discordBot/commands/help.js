b13bb403e558c093a2d1d76ce5c8e8963b74ac31
Danish Khan <61810778+PLOKMJNB@users.noreply.github.com>
2020-12-18 11:25:40 -0500

    Update help.js

--- src/components/discordBot/commands/help.js
+++ src/components/discordBot/commands/help.js
@@ -21,6 +21,7 @@ module.exports = {
         ];
         const outMsg = new RichEmbed({
             color: 0x4287F5,
+            thumbnail: "https://raw.githubusercontent.com/Heat-Gaming/assets/master/server%20images/sv1.png",
             description: descLines.join('\n')
         });
         return await message.channel.send(outMsg);
